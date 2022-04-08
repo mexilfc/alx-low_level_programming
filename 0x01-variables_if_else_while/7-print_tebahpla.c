cc5404c5338df75e108e2b05221dc34e3879be6a
mexilfc <messaylfc@gmail.com>
2022-04-08 13:15:46 -0700

    This is it

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,20 +1,17 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <time.h>
 /**
  * main - Entry point
  *
  * Description: Print lowercase alphabet backwards
  * Return: 0 
  */
-
 int main(void)
 {
 char c;
-
 for (c = 'z'; c >= 'a'; --c)
 putchar(c);
-
-utchar('\n');
-
+putchar('\n');
 return (0);
 }
