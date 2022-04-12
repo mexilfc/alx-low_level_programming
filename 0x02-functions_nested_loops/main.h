b8a5384e4a85a21b0d90cf29619b97bdd1fec0e3
mexilfc <messaylfc@gmail.com>
2022-04-12 11:57:37 -0700

    yolo

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,9 +1,9 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
-int _putchar(char c};
+int _putchar(char c);
 void print_alphabet(void);
-void print_alphabet_x10(void)
+void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
