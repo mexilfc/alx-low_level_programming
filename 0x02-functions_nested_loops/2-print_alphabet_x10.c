b921696ab8cc07b64662fdfcdc4a7bca171945b9
mexilfc <messaylfc@gmail.com>
2022-04-12 12:21:44 -0700

    task 1

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,12 +8,12 @@
 void print_alphabet_x10(void)
 {
 int count = 0;
-char i;
+char letter;
 
 	while (count++ <= 9)
 {
-	for (i = 'a'; i <= 'z'; i++)
-		_putchar(i);
+	for ( letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
 		_putchar('\n');
 }
 }
